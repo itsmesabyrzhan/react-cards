795c94b4f8d27b30de0e04b00a83bbbf8026a0ec
Sabyrzhan <itsme.sabyrzhan@gmail.com>
2025-10-24 18:59:14 +0500

    feat: preparing for the app, setting up prettier

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -19,5 +20,14 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: prettier,
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        printWidth: 120,
+        tabWidth: 2,
+      },
+    ],
   },
 ])
